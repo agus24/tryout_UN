e5271c69ad83ede99194071a79eb4a73b3ee40f7
plonknimbuzz <plonkimbuzz@gmail.com>
2017-03-12 19:23:25 +0700

    Update leech_link.php

--- library/leech_link.php
+++ library/leech_link.php
@@ -3,37 +3,37 @@
 	need install tidy first, because too many invalid html
 */
 require("simple_html_dom.php");
-$link = "test.html";
-$html = file_get_contents($link);
+$link = "http://tryoutunonline.com/index.php";
+$html = file_get_html($link); 
 
-$config = array(
-   'indent'         => true,
-   //'output-xhtml'   => true,
-   'wrap'           => 200);
-$tidy = new tidy;
-$tidy->parseString($html, $config, 'utf8');
-$tidy->cleanRepair();
-//echo ($tidy);
-$html = str_get_html($html); 
-//$html = file_get_html("clean.html"); 
-//var_dump( $html);
 $data = array();
 foreach($html->find('div[class="collapse navbar-collapse main-nav"]') as $e) 
 {
-	$tmp = array("bidstudi"=>'','smp'=>0);
 	foreach($e->find('li') as $f)
 	{
-		$tmp['bidstudi'] = trim($f->find('a',0)->plaintext);
-		$tab = str_replace("#","",$f->find('a',0)->href);
-		
+		$bidStudi = trim($f->find('a',0)->plaintext);
+		//$tab = str_replace("#","",$f->find('a',0)->href);echo $tab;
+		$tab = $f->find('a',0)->href;
+		foreach($html->find($tab) as $e)
+		{
+			foreach($e->find('div[class="col-md-3"]') as $f)
+			{
+				$kelas = $f->find('h3[class="box-title"]', 0)->innertext;
+				foreach($f->find('a') as $g)
+				{
+					$judul = $g->innertext;
+					$url = $g->href;
+					$data[] = array(
+						"kelas"=> $kelas,
+						"bidstudi"=> $bidStudi,
+						"judul"=> $judul,
+						"url"=> "http://tryoutunonline.com/".$url,
+					);
+				}
+			}
+		}
 	}
 }
-foreach($html->find('#1') as $e)
-{
-	foreach($e->find('div[class="col-md-3"]')->$f)
-	{
-		
-	}
-}
-   // print_r($soal);
+
+   print_r($data);
 ?>
