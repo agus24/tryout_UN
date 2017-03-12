0981a30cbac4939543b5335de30467e28e02643b
agus24 <agusx244@gmail.com>
2017-03-12 15:16:58 +0700

    tambahin db_structure

--- library/leech_data.php
+++ library/leech_data.php
@@ -10,20 +10,21 @@ $config = array(
    'indent'         => true,
    'output-xhtml'   => true,
    'wrap'           => 200);
+
 $tidy = new tidy;
 $tidy->parseString($html, $config, 'utf8');
 $tidy->cleanRepair();
 
 $html = str_get_html($tidy);
 $soal = array();
-foreach($html->find('div[class^="soal_ujian page"]') as $e) 
+foreach($html->find('div[class^="soal_ujian page"]') as $e)
 {
 	$temp= array('pertanyaan'=>'', 'jawaban'=>array(), 'kunci'=>'');
 	foreach($e->find('div[class="pertanyaan"]') as $f)
 	{
 		$temp['pertanyaan'] = preg_replace('/\s+/', ' ',strip_tags($f->innertext));
 	}
-	
+
 	foreach($e->find('table') as $f)
 	{
 		$a=0;
@@ -32,7 +33,7 @@ foreach($html->find('div[class^="soal_ujian page"]') as $e)
 			if($a!=0) {
 				$temp['jawaban'][] = str_replace('&nbsp;','', trim($g->find('td',1)->innertext()));
 				if( $g->find('td',0)->find('div',0)->find('input',0)->getAttribute('value')==1) $temp['kunci']=$a;
-			
+
 			}
 			$a++;
 		}
