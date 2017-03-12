8d3ea1bb7ddb54587716d01c5bb65b581c3e9421
plonknimbuzz <plonkimbuzz@gmail.com>
2017-03-12 14:33:49 +0700

    Update leech_data.php

--- library/leech_data.php
+++ library/leech_data.php
@@ -1,1 +1,43 @@
-asdasd
+<?php
+/*
+	need install tidy first, because too many invalid html
+*/
+require("simple_html_dom.php");
+$link = "http://tryoutunonline.com/soal-86-prediksi_soal_usbn.html";
+$html = file_get_contents($link);
+
+$config = array(
+   'indent'         => true,
+   'output-xhtml'   => true,
+   'wrap'           => 200);
+$tidy = new tidy;
+$tidy->parseString($html, $config, 'utf8');
+$tidy->cleanRepair();
+
+$html = str_get_html($tidy);
+$result = array();
+foreach($html->find('div[class^="soal_ujian page"]') as $e) 
+{
+	$temp= array('soal'=>'', 'jawaban'=>array(), 'kunci'=>'');
+	foreach($e->find('div[class="pertanyaan"]') as $f)
+	{
+		$temp['soal'] = strip_tags($f->innertext);
+	}
+	
+	foreach($e->find('table') as $f)
+	{
+		$a=0;
+		foreach($f->find('tr') as $g)
+		{
+			if($a!=0) {
+				$temp['jawaban'][] = $g->find('td',1)->innertext();
+				if( $g->find('td',0)->find('div',0)->find('input',0)->getAttribute('value')==1) $temp['kunci']=$a;
+			
+			}
+			$a++;
+		}
+	}
+	$result[] = $temp;
+}
+    print_r($result);
+?>
